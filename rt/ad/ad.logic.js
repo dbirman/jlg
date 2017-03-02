0ebc34fe25d2f2ffa51c1c475e159245c5da9f5d
Dan Birman <danbirman@gmail.com>
2017-03-02 09:16:53 -0800

    Ad is functional

--- rt/ad/ad.logic.js
+++ rt/ad/ad.logic.js
@@ -1,5 +1,14 @@
+// This is the ad logic. You shouldn't need to edit this unless you're running your server somewhere other than the gru.stanford.edu server (which you shouldn't do).
+
+var debug;
+
+function getExperiment() {
+	debug = getQueryVariable('debug')=='true';
+}
+
 window.onload = function() {
-	if (turk.assignmentId=="" || turk.previewMode) {
+	debug = getQueryVariable('debug')=='true';
+	if (!debug && turk.assignmentId=="" || turk.previewMode) {
 		document.getElementById("preview").style.display="";
 		document.getElementById("active").style.display="none";
 	} else {
@@ -11,5 +20,17 @@ window.onload = function() {
 function openwindow() {
 	var path = location.pathname;
 	var expName = path.substr(path.indexOf('ad-')+3,path.indexOf('.html')-1);
-	popup = window.open('localhost:8080/exp.html?'+expName,'Popup','toolbar=no,status=no,menubar=no,scrollbars=yes,resizable=no,width='+1024+',height='+768+'');
+	popup = window.open('http://localhost:8080/exp.html?exp='+expName,'Popup','toolbar=no,status=no,menubar=no,scrollbars=yes,resizable=no,width='+1024+',height='+768+'');
+}
+
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+    console.log('Query variable %s not found', variable);
 }
